72a2f1f7b056e2fc185e4e25660924194b06c5cd
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-07-19 15:47:00 -0400

    add watch feature for jobs. when passing --watch to 'bpcli job get', bpcli will return the job every five seconds until it has either passed or failed

--- cmd/job_get.go
+++ cmd/job_get.go
@@ -3,6 +3,10 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/BlueMedoraPublic/bpcli/bindplane/sdk"
 
 	"github.com/spf13/cobra"
 )
@@ -18,18 +22,40 @@ var getJobCmd = &cobra.Command{
 func init() {
 	jobCmd.AddCommand(getJobCmd)
 	getJobCmd.Flags().StringVar(&jobID, "id", "", "The ID of the job")
+	getJobCmd.Flags().BoolVarP(&watch, "watch", "", false, "Monitor the job's status in the forground until it has passed or failed.")
 	getJobCmd.MarkFlagRequired("id")
 }
 
 func getJob() {
-	j, err := bp.GetJob(jobID)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
+	for {
+		j, err := bp.GetJob(jobID)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 
-	if err := j.Print(jsonFmt); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		if err := j.Print(jsonFmt); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		// exit if watch not passed
+		if watch == false {
+			os.Exit(0)
+		}
+
+		// exit if job is finished
+		if jobFinished(j) == true {
+			os.Exit(0)
+		}
+		time.Sleep(time.Second * 5)
 	}
+}
+
+// return true if job is complete (passed or failed)
+func jobFinished(j sdk.Job) bool {
+	if strings.ToLower(j.Status) == "in progress" {
+		return false
+	}
+	return true
 }
--- cmd/root.go
+++ cmd/root.go
@@ -15,6 +15,7 @@ import (
 var sourceFile string
 var credentialFile string
 var jsonFmt bool
+var watch bool
 
 // uuid flags
 var jobID string
