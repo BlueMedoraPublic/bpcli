2222ebbd459877c16c845fc7951e1858de0e685e
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-08-13 17:00:51 -0400

    update homeDir function to return the entire file path, and allow this to be set by an env variable

--- config/config_test.go
+++ config/config_test.go
@@ -1,39 +1,41 @@
 package config
 
 import (
-	"os/exec"
+	//"os/exec"
+	"os"
+	"log"
 	"testing"
 )
+
+const configFile = "/tmp/.bpcli"
 
 //Writes tests for all functions within config.go
 
 //Will need to execute a bash script to set the file being tested back to
 //it's original state
-var delScript = exec.Command("/bin/sh", dir+"/test-file-delete")
-var resetScript = exec.Command("/bin/sh", dir+"/test-file-reset")
+//var delScript = exec.Command("/bin/sh", dir+"/test-file-delete")
+//var resetScript = exec.Command("/bin/sh", dir+"/test-file-reset")
 
 // TestListAccounts tests for missing/non-existing config file
 func TestListAccounts(t *testing.T) {
-	//Remove the .bpcli file for testing
-	delScript.Run()
+
+	os.Setenv("BINDPLANE_CONFIG_FILE", configFile)
+	createConfigFile()
 
 	err := ListAccounts()
 	if err == nil {
-		t.Errorf("The function should return an error")
+		t.Errorf(err.Error())
 	}
-
-	// Reset .bpcli file
-	resetScript.Run()
 }
 
 // TestListAccounts2 tests for any errors when a file exists
-func TestListAccounts2(t *testing.T) {
+/*func TestListAccounts2(t *testing.T) {
 
 	err := ListAccounts()
 	if err != nil {
 		t.Errorf("The function should not be returning an error if file exists")
 	}
-}
+}*/
 
 // TestRemove tests the remove function in config.go
 // func TestRemove(t *testing.T) {
@@ -42,3 +44,17 @@ func TestListAccounts2(t *testing.T) {
 // 		t.Errorf("This file does not exist, but the function should not return an error")
 // 	}
 // }
+
+
+func createConfigFile() {
+	emptyFile, err := os.Create(configFile)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	emptyFile.Close()
+}
+
+func cleanconfigFile() {
+	
+}
