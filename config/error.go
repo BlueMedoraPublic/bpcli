ff953bb31f2c5fb4d474f709075519330508031d
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-08-20 16:22:32 -0400

    Add account not found error

--- config/error.go
+++ config/error.go
@@ -9,3 +9,7 @@ func fileNotFoundError() error {
         "Use the `bpcli account add` command to generate the file "+
         "and add an account to the list")
 }
+
+func accountNotFoundError(name string) error {
+    return errors.New("The account '" + name + "' does not exist in the credentials file")
+}
