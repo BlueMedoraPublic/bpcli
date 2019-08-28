d02caaf4d0b656ff73fef7dae73930b0398ff128
Joseph Sirianni <joe.sirianni@bluemedora.com>
2019-08-28 10:20:08 -0400

    test uniqueUUID

--- config/unique_test.go
+++ config/unique_test.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+    "testing"
+)
+
+func TestUniqueUUID(t *testing.T) {
+    // using fake UUIDs generated from https://www.uuidgenerator.net/
+    u := "459371a9-73df-4a0b-b212-e141ef4e3136"
+    a := []account{}
+    a = append(a, account{Name: "bob", Key: u})
+
+    x, err := uniqueUUID(a, u)
+    if err != nil {
+        t.Errorf("Expected uniqueUUID() to return a nil error, got: " + err.Error())
+        return
+    }
+    if x == true {
+        t.Errorf("Expected uniqueUUID() to return false when passing in a UUID that is already in the account list")
+    }
+}
