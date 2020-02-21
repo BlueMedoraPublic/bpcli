bd4f39bc374dd70d1f48316beb4acfa18cc09416
Joseph Sirianni <joe.sirianni@bluemedora.com>
2020-02-21 11:36:17 -0500

    remove functions that take more than version as an argument, remain consistent

--- bindplane/api/logs.go
+++ bindplane/api/logs.go
@@ -13,12 +13,6 @@ func GetLogSourceTypesPath(version string) string {
 	return version + logSourceTypesPath
 }
 
-// GetLogSourceTypeParamsPath returns the api path for a specific
-// log source's parameters endpoint
-func GetLogSourceTypeParamsPath(version, sourceID string) string {
-	return GetLogSourceTypesPath(version) + "/" + sourceID + "/parameters"
-}
-
 // GetLogSourceConfigsPath returns the api path for all log
 // source configs endpoint
 func GetLogSourceConfigsPath(version string) string {
@@ -37,57 +31,10 @@ func GetLogAgentInstallCommandPath(version string) string {
 	return GetLogAgentsAllPath(version) + "/install_commands"
 }
 
-// GetLogAgentPath returns the api path for a specific agent
-func GetLogAgentPath(version, agentID string) string {
-	return GetLogAgentsAllPath(version) + "/" + agentID
-}
-
-// GetLogAgentTaskDetailsPath returns the api path for
-// getting a log agent task
-func GetLogAgentTaskDetailsPath(version, agentID, taskID string) string {
-	return GetLogAgentPath(version, agentID) + "/tasks/" + taskID
-}
-
-// GetLogAgentSourcesPath returns the api path for describing
-// a given log agent's sources
-func GetLogAgentSourcesPath(version, agentID string) string {
-	return GetLogAgentPath(version, agentID) + "/sources"
-}
-
-// GetLogAgentDestinationsPath returns the api path for describing
-// a given log agent's destinations
-func GetLogAgentDestinationsPath(version, agentID string) string {
-	return GetLogAgentPath(version, agentID) + "/destinations"
-}
-
-// GetLogAgentUpdatePath returns the api path for updating
-// a given log agent's version
-func GetLogAgentUpdatePath(version, agentID string) string {
-	return GetLogAgentPath(version, agentID) + "/update_agent"
-}
-
-// GetLogAgentDeploySourceConfigPath returns the api path for
-// deploying a source config ot an agent
-func GetLogAgentDeploySourceConfigPath(version, agentID string) string {
-	return GetLogAgentPath(version, agentID) + "/deploy_source_config"
-}
-
-// GetLogAgentDeployDestinationConfigPath returns the api path for
-// deploying a source config ot an agent
-func GetLogAgentDeployDestinationConfigPath(version, agentID string) string {
-	return GetLogAgentPath(version, agentID) + "/deploy_destination_config"
-}
-
 // GetLogDestinationTypesPath returns the api path for
 // log listing log destinations
 func GetLogDestinationTypesPath(version string) string {
 	return version + logDestinationTypesPath
-}
-
-// GetLogDestinationTypesParametersPath returns the api path for
-// getting a destination type's parameters
-func GetLogDestinationTypesParametersPath(version, destID string) string {
-	return GetLogDestinationTypesPath(version) + "/" + destID + "/parameters"
 }
 
 // GetLogDestinationConfigsAllPath returns the api path for
@@ -96,26 +43,8 @@ func GetLogDestinationConfigsAllPath(version string) string {
 	return version + logDestinationConfigsPath
 }
 
-// GetLogDestinationConfigPath returns the api path for
-// getting the config for a given destination
-func GetLogDestinationConfigPath(version, destID string) string {
-	return GetLogDestinationConfigsAllPath(version) + "/" + destID
-}
-
-// GetLogDestinationUpdatePath returns the api path for
-// updating an existing destination
-func GetLogDestinationUpdatePath(version, destID string) string {
-	return GetLogDestinationConfigsAllPath(version) + "/" + destID + "/update_destination"
-}
-
 // GetLogTemplatesAllPath returns the api path for listing
 // all templates
 func GetLogTemplatesAllPath(version string) string {
 	return version + logTemplatesPath
-}
-
-// GetLogTemplatePath returns the api path for a specific
-// template
-func GetLogTemplatePath(version, templateID string) string {
-	return version + logTemplatesPath + "/" + templateID
 }
--- bindplane/api/logs_test.go
+++ bindplane/api/logs_test.go
@@ -38,13 +38,6 @@ func TestGetLogsourcetypesPath(t *testing.T) {
 	}
 }
 
-func TestGetLogSourceTypeParamsPath(t *testing.T) {
-	x := GetLogSourceTypeParamsPath(apiVersion, "mysql")
-	if x != (apiVersion + logSourceTypesPath + "/mysql/parameters") {
-		t.Errorf("Expected GetLogSourceTypeParamsPath() to return " + apiVersion + logSourceTypesPath + "/mysql/parameters")
-	}
-}
-
 func TestGetLogSourceConfigsPath(t *testing.T) {
 	x := GetLogSourceConfigsPath(apiVersion)
 	if x != apiVersion+logSourceConfigsPath {
@@ -66,55 +59,6 @@ func TestGetLogAgentInstallCommandPath(t *testing.T) {
 	}
 }
 
-func TestGetLogAgentPath(t *testing.T) {
-	x := GetLogAgentPath(apiVersion, "abc")
-	if x != GetLogAgentsAllPath(apiVersion)+"/"+"abc" {
-		t.Errorf("Expected GetLogAgentsAllPath() to return " + GetLogAgentsAllPath(apiVersion) + "/" + "abc")
-	}
-}
-
-func TestGetLogAgentTaskDetailsPath(t *testing.T) {
-	x := GetLogAgentTaskDetailsPath(apiVersion, "abc", "xyz")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/tasks/"+"xyz" {
-		t.Errorf("Expected GetLogAgentTaskDetailsPath() to return " + GetLogAgentPath(apiVersion, "abc") + "/tasks/" + "xyz")
-	}
-}
-
-func TestGetLogAgentSourcesPath(t *testing.T) {
-	x := GetLogAgentSourcesPath(apiVersion, "abc")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/sources" {
-		t.Errorf("Expected GetLogAgentSourcesPath() to return " + GetLogAgentPath(apiVersion, "abc") + "/sources")
-	}
-}
-
-func TestGetLogAgentDestinationsPath(t *testing.T) {
-	x := GetLogAgentDestinationsPath(apiVersion, "abc")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/destinations" {
-		t.Errorf("Expected GetLogAgentDestinationsPath() to return " + GetLogAgentPath(apiVersion, "abc") + "/destinations")
-	}
-}
-
-func TestGetLogAgentUpdatePath(t *testing.T) {
-	x := GetLogAgentUpdatePath(apiVersion, "abc")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/update_agent" {
-		t.Errorf("Expected GetLogAgentUpdatePath() to return " + GetLogAgentPath(apiVersion, "abc") + "/update_agent")
-	}
-}
-
-func TestGetLogAgentDeploySourceConfigPath(t *testing.T) {
-	x := GetLogAgentDeploySourceConfigPath(apiVersion, "abc")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/deploy_source_config" {
-		t.Errorf("Expected GetLogAgentDeploySourceConfigPath() to return" + GetLogAgentPath(apiVersion, "abc") + "/deploy_source_config")
-	}
-}
-
-func TestGetLogAgentDeployDestinationConfigPath(t *testing.T) {
-	x := GetLogAgentDeployDestinationConfigPath(apiVersion, "abc")
-	if x != GetLogAgentPath(apiVersion, "abc")+"/deploy_destination_config" {
-		t.Errorf("Expected GetLogAgentDeployDestinationConfigPath() to return " + GetLogAgentPath(apiVersion, "abc") + "/deploy_destination_config")
-	}
-}
-
 func TestGetLogDestinatinationTypesPath(t *testing.T) {
 	x := GetLogDestinationTypesPath(apiVersion)
 	if x != apiVersion+logDestinationTypesPath {
@@ -122,30 +66,9 @@ func TestGetLogDestinatinationTypesPath(t *testing.T) {
 	}
 }
 
-func TestGetLogDestinationTypesParametersPath(t *testing.T) {
-	x := GetLogDestinationTypesParametersPath(apiVersion, "abc")
-	if x != GetLogDestinationTypesPath(apiVersion)+"/abc/parameters" {
-		t.Errorf("Expected GetLogDestinationTypesParametersPath() to return " + GetLogDestinationTypesPath(apiVersion) + "abc")
-	}
-}
-
 func TestGetLogDestinationConfigsAllPath(t *testing.T) {
 	x := GetLogDestinationConfigsAllPath(apiVersion)
 	if x != apiVersion+logDestinationConfigsPath {
 		t.Errorf("Expected GetLogDestinationConfigsAllPath() to return " + apiVersion + logDestinationConfigsPath)
-	}
-}
-
-func TestGetLogDestinationConfigPath(t *testing.T) {
-	x := GetLogDestinationConfigPath(apiVersion, "abc")
-	if x != GetLogDestinationConfigsAllPath(apiVersion)+"/abc" {
-		t.Errorf("Expected GetLogDestinationConfigPath() to return " + GetLogDestinationConfigsAllPath(apiVersion) + "/abc")
-	}
-}
-
-func TestGetLogDestinationUpdatePath(t *testing.T) {
-	x := GetLogDestinationUpdatePath(apiVersion, "abc")
-	if x != GetLogDestinationConfigsAllPath(apiVersion)+"/abc/update_destination" {
-		t.Errorf("Expected GetLogDestinationUpdatePath() to return" + GetLogDestinationConfigsAllPath(apiVersion) + "/abc/update_destination")
 	}
 }
