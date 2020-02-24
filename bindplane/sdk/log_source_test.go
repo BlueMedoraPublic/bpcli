fd49d74d821289d507dec8579c2a0939dc10492b
Joseph Sirianni <joe.sirianni@bluemedora.com>
2020-02-24 12:47:45 -0500

    finish testing LogSourceConfig ValidateCreate()

--- bindplane/sdk/log_source_test.go
+++ bindplane/sdk/log_source_test.go
@@ -4,10 +4,9 @@ import (
     "testing"
 )
 
-func TestValidateCreateGood(t *testing.T) {
+func testPopulatedLogSourceConfig() LogSourceConfig {
     x := newLogSourceConfig()
-
-    x.ID = "" // id should be empty when creating a source
+    x.ID = "abc"
     x.Name = "abc"
     x.Source.ID = "abc"
     x.Source.Name = "abc"
@@ -15,25 +14,66 @@ func TestValidateCreateGood(t *testing.T) {
     x.Configuration["ReadFromHead"] = true
     x.Configuration["ReadInterval"] = 2
     x.Configuration["MaxReads"] = "2"
+    return x
+}
 
+func TestValidateCreateGood(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
     if err := x.ValidateCreate(); err != nil {
-        t.Errorf("Expected ValidateCreate() to return a nil error, got: " + err.Error())
+        t.Errorf("Expected ValidateCreate() to return a nil error when ID is empty, got: " + err.Error())
     }
 }
 
 func TestValidateCreateBadID(t *testing.T) {
-    x := newLogSourceConfig()
-
-    x.ID = "abc" // id should be empty when creating a source
-    x.Name = "abc"
-    x.Source.ID = "abc"
-    x.Source.Name = "abc"
-    x.Source.Version = "1.0"
-    x.Configuration["ReadFromHead"] = true
-    x.Configuration["ReadInterval"] = 2
-    x.Configuration["MaxReads"] = "2"
-
+    x := testPopulatedLogSourceConfig()
+    x.ID = "abc"
     if err := x.ValidateCreate(); err == nil {
-        t.Errorf("Expected ValidateCreate() to return an error when ID is set, got nil")
+        t.Errorf("Expected ValidateCreate() to return an error when ID is not set, got nil")
+    }
+}
+
+func TestValidateCreateBadName(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
+    x.Name = ""
+    if err := x.ValidateCreate(); err == nil {
+        t.Errorf("Expected ValidateCreate() to return an error when Name is not set, got nil")
+    }
+}
+
+func TestValidateCreateBadSourceID(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
+    x.Source.ID = ""
+    if err := x.ValidateCreate(); err == nil {
+        t.Errorf("Expected ValidateCreate() to return an error when Source.ID is not set, got nil")
+    }
+}
+
+func TestValidateCreateBadSourceName(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
+    x.Source.Name = ""
+    if err := x.ValidateCreate(); err == nil {
+        t.Errorf("Expected ValidateCreate() to return an error when Source.Name is not set, got nil")
+    }
+}
+
+func TestValidateCreateBadSourceVersion(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
+    x.Source.Version = ""
+    if err := x.ValidateCreate(); err == nil {
+        t.Errorf("Expected ValidateCreate() to return an error when Source.Version is not set, got nil")
+    }
+}
+
+func TestValidateCreateBadConfiguration(t *testing.T) {
+    x := testPopulatedLogSourceConfig()
+    x.ID = ""
+    x.Configuration = make(map[string]interface{})
+    if err := x.ValidateCreate(); err == nil {
+        t.Errorf("Expected ValidateCreate() to return an error when Configuration is an empty map, got nil")
     }
 }
